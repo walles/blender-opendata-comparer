c55398a510c9543bbdfbec679dce8adc7ca2a6c7
Johan Walles <johan.walles@gmail.com>
2020-02-21 19:57:36 +0100

    Extract more detailed data
    And crash.
    
    We might need to start looking at explicit support for different schema
    versions.

--- wrangle.py
+++ wrangle.py
@@ -13,6 +13,8 @@ def process_entry(entry: Dict):
     if type(data) is not list:
         data = [data]
     for data_point in data:
+        blender_version = data_point["blender_version"]["version"]
+        operating_system = data_point["system_info"]["system"]
         compute_devices = data_point["device_info"]["compute_devices"]
         if len(compute_devices) != 1:
             # Multiple compute devices, never mind
@@ -21,7 +23,22 @@ def process_entry(entry: Dict):
         compute_device = compute_devices[0]
         if type(compute_device) is dict:
             compute_device = compute_device["name"]
+        if compute_device != "AMD Ryzen 5 3500U with Radeon Vega Mobile Gfx":
+            continue
+        # FIXME: Look at only CPU devices
         print(compute_device)
+
+        # FIXME: For each scene...
+        for scene in data_point["scenes"]:
+            scene_name = scene["name"]
+            render_time_seconds = scene["stats"]["total_render_time"]
+            t = {
+                "blender_version": blender_version,
+                "os": operating_system,
+                "scene": scene_name,
+                "render_time_s": render_time_seconds,
+            }
+            pprint.pprint(t)
 
 
 def process_opendata(jsonl):
