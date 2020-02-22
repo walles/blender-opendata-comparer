78095b6cef58f84a252b735aecb7d15cb16d3755
Johan Walles <johan.walles@gmail.com>
2020-02-22 09:57:52 +0100

    Redo everything
    For improving schema support.

--- wrangle.py
+++ wrangle.py
@@ -4,56 +4,76 @@ import sys
 import json
 import pprint
 import zipfile
+import traceback
 
-from typing import Dict
+from typing import Dict, NamedTuple, List, Iterable
 
 
-def process_entry(entry: Dict):
+class Sample(NamedTuple):
+    blender_version: str
+    os_name: str
+    device_name: str
+    device_type: str
+    scene_name: str
+    render_time_seconds: float
+
+
+def process_entry_v1(entry: Dict) -> List[Sample]:
     data = entry["data"]
-    if type(data) is not list:
-        data = [data]
-    for data_point in data:
-        blender_version = data_point["blender_version"]["version"]
-        operating_system = data_point["system_info"]["system"]
-        compute_devices = data_point["device_info"]["compute_devices"]
-        if len(compute_devices) != 1:
-            # Multiple compute devices, never mind
-            return
+    blender_version = data["blender_version"]["version"]
+    operating_system = data["system_info"]["system"]
+    compute_devices = data["device_info"]["compute_devices"]
+    device_type = data["device_info"]["device_type"]
+    if len(compute_devices) != 1:
+        # Multiple compute devices, never mind
+        return []
 
-        compute_device = compute_devices[0]
-        if type(compute_device) is dict:
-            compute_device = compute_device["name"]
-        if compute_device != "AMD Ryzen 5 3500U with Radeon Vega Mobile Gfx":
-            continue
-        # FIXME: Look at only CPU devices
-        print(compute_device)
+    compute_device = compute_devices[0]
+    # FIXME: Look at only CPU devices
+    print(compute_device)
 
-        # FIXME: For each scene...
-        for scene in data_point["scenes"]:
-            scene_name = scene["name"]
-            render_time_seconds = scene["stats"]["total_render_time"]
-            t = {
-                "blender_version": blender_version,
-                "os": operating_system,
-                "scene": scene_name,
-                "render_time_s": render_time_seconds,
-            }
-            pprint.pprint(t)
+    samples: List[Sample] = []
+    for scene in data["scenes"]:
+        scene_name = scene["name"]
+        render_time_seconds = scene["stats"]["total_render_time"]
+        samples.append(
+            Sample(
+                blender_version=blender_version,
+                os_name=operating_system,
+                device_name=compute_device,
+                device_type=device_type,
+                scene_name=scene_name,
+                render_time_seconds=render_time_seconds,
+            )
+        )
+    return samples
 
 
-def process_opendata(jsonl):
+def process_opendata(jsonl: Iterable[bytes]) -> List[Sample]:
+    samples: List[Sample] = []
     for line in jsonl:
         entry = json.loads(line)
         try:
-            process_entry(entry)
+            if entry["schema_version"] == "v1":
+                samples += process_entry_v1(entry)
+            else:
+                pprint.pprint(entry, stream=sys.stderr)
+                sys.exit("Unsupported schema version")
         except Exception as e:
-            print(e, file=sys.stderr)
             pprint.pprint(entry, stream=sys.stderr)
+            traceback.print_exc(file=sys.stderr)
+            sys.exit(1)
+
+    return samples
 
 
+samples: List[Sample] = []
 with zipfile.ZipFile("opendata-2020-02-21-063254+0000.zip") as opendata:
     for entry in opendata.infolist():
         if not entry.filename.endswith(".jsonl"):
             continue
         with opendata.open(entry) as jsonl:
-            process_opendata(jsonl.readlines())
+            samples += process_opendata(jsonl.readlines())
+
+for sample in samples:
+    pprint.pprint(sample)
